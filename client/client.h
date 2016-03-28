ec6d954fe37cd4fad4f5c4c37c04c2de116a48e3
kopkan <375291152416@yandex.ru>
2016-03-28 15:50:03 +0300

    Update client.h

--- client/client.h
+++ client/client.h
@@ -8,8 +8,9 @@ namespace corteli {
 
 	public:
 		Client();
+		Client(int port, char* ip = 0);
 		~Client();
-		int bind(char* ip, int port = 0);
+		int bind(int port, char* ip = 0);
 		int connect(char* ip, int port = 0);
 		int send(char* buff, int size = -1);
 		int recv(char* buff, int size);
@@ -17,23 +18,31 @@ namespace corteli {
 
 
 		bool connectCycle(char* ip, int port, int limNoCon = -1, int timeLimit = -1, int sleepBetweenTry = 100);
-		void recvCycle(char* buff, int size);
+		void recvCycle(int size);
 
-		boost::thread* recvCycleT(char* buff, int size);
+		boost::thread* recvCycleT(int size);
 
 		bool checkOnline();
 		void checkOnlineCycle();
 
 		boost::thread* checkOnlineCycleT();
 
-
-		virtual void newMessage(char* buff, int size);
-		void start(char* buff, int size, int time);
+		void start(char* ip, int port, int buffSize, int time);
+		void close();
 
 	private:
 		boost::asio::io_service* io_service;
 		boost::asio::ip::tcp::socket* clientSocket;
+
+		boost::thread* checkOnlineT;
+		boost::thread* recvT;
+
 		boost::system::error_code err;
 		int lastMesTime;
+		char* buff;
+
+		void _init();
+		virtual void _newMessage(char* buff, int size);
+		virtual void _endConnection();
 	};
 };
