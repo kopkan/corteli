7a6ca8b775fa3676101819db8ab00c25dac2f5c6
kopkan <375291152416@yandex.ru>
2016-04-04 01:58:12 +0300

    Update client.h

--- client/client.h
+++ client/client.h
@@ -1,48 +1,58 @@
 #pragma once
+
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
 
+#include "Header.h"
+
 namespace corteli {
 
-	class Client {
+	class BaseClient {
 
 	public:
-		Client();
-		Client(int port, char* ip = 0);
-		~Client();
-		int bind(int port, char* ip = 0);
-		int connect(char* ip, int port = 0);
+		BaseClient(boost::asio::io_service* ioService = NULL);
+		~BaseClient();
+
 		int send(char* buff, int size = -1);
-		int recv(char* buff, int size);
 
+		int start();
+		int startT();
 
+		int conInfo(boost::asio::ip::tcp::endpoint remoteAddr, int recvBuffSize, boost::asio::ip::tcp::endpoint localAddr);
 
-		bool connectCycle(char* ip, int port, int limNoCon = -1, int timeLimit = -1, int sleepBetweenTry = 100);
-		void recvCycle(int size);
-
-		boost::thread* recvCycleT(int size);
-
-		bool checkOnline();
-		void checkOnlineCycle();
-
-		boost::thread* checkOnlineCycleT();
-
-		void start(char* ip, int port, int buffSize, int time);
 		void close();
 
-	private:
-		boost::asio::io_service* io_service;
-		boost::asio::ip::tcp::socket* clientSocket;
+	protected:
+		boost::asio::io_service* _ioService;
+		boost::asio::ip::tcp::socket* _clientSocket;
+		boost::asio::ip::tcp::endpoint _localAddr;
+		boost::asio::ip::tcp::endpoint _remoteAddr;
 
-		boost::thread* checkOnlineT;
-		boost::thread* recvT;
+		bool _isNewIoService=false;
+		bool _isWork=false;
+		int _error = 0;
 
-		boost::system::error_code err;
-		int lastMesTime;
-		char* buff;
 
-		void _init();
+		int _status=0;
+		int _recvBuffSize;
+
+		boost::thread* _workT;
+
+		void _init(boost::asio::io_service* ioService);
+		int _bind(boost::asio::ip::tcp::endpoint localAddr);
+		int _connect(boost::asio::ip::tcp::endpoint remoteAddr);
+		bool _connectCycle(boost::asio::ip::tcp::endpoint remoteAddr, int limNoCon = -1, int timeLimit = -1, int sleepBetweenTry = 100);
+		int _recv(char* buff, int buffSize);
+		void _recvCycle(int buffSize);
+		void _recvCycleT(int buffSize);
+
+		virtual int _expansion();
+		int _destroy();
+
 		virtual void _newMessage(char* buff, int size);
-		virtual void _endConnection();
+		virtual void _endWork();
+
+
+
 	};
 };
