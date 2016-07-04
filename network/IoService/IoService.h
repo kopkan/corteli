4d5d0d634ac8299f954c0e96d151ce1093a5f5a2
kopkan <375291152416@yandex.ru>
2016-07-05 00:51:26 +0300

    Add files via upload

---
+++ network/IoService/IoService.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <thread>
+
+#include "D:/Develop/VisualStudio15Project/corteli/Base/BaseObject/BaseObject/BaseObject.h"
+#pragma comment(lib, "D:/Develop/VisualStudio15Project/corteli/Base/BaseObject/Release/BaseObject.lib")
+
+
+namespace corteli {
+	namespace network {
+
+		class IoService
+		{
+		public:
+			enum status
+			{
+				STOPPED = 0, RUN = 1, STOP = 2
+			};
+
+
+			IoService(bool started = false);
+			~IoService();
+			void start();
+			void startT();
+			boost::asio::io_service& getIoSevice();
+			void stop();
+			status getStatus();
+
+		private:
+			status _status = status::STOPPED;
+			boost::asio::io_service _ioService;
+		};
+
+	}
+}
+
+#pragma comment(lib, "D:/Develop/VisualStudio15Project/corteli/network/IoService/Release/IoService.lib")
