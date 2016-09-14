39f91cdf7e375e3a9ab7687d1124e37725697b9e
kopkan <375291152416@yandex.ru>
2016-09-14 20:35:39 +0300

    Create Client.h

---
+++ HelpNetwork/Client.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <boost\asio.hpp>
+#include <memory>
+#include <iostream>
+#include <atomic>
+#include "..\..\..\..\corteli\Base\BaseContainers\BaseContainers\Buffer.h"
+
+class Client
+	: public std::enable_shared_from_this<Client>
+{
+public:
+	Client(boost::asio::io_service& io);
+	Client(boost::asio::ip::tcp::socket socket);
+	~Client();
+	time_t getlastMessageTime();
+	void connect(boost::asio::ip::tcp::endpoint endpoint);
+	void startRecive();
+	int close();
+	void onRecive(std::function<void(corteli::base::container::Buffer<char>)> recvFunc);
+	void onClose(std::function<void()> closeFunc);
+	void onConnect(std::function<void()> connectFunc);
+	void send(corteli::base::container::Buffer<char> message);
+
+private:
+	void recive_();
+
+	boost::asio::ip::tcp::socket socket_;
+	std::atomic<time_t> lastRecvMessageTime_ = 0;
+	corteli::base::container::Buffer<char> recvBuff_;
+
+	std::function<void()> connectFunc_ = nullptr;
+	std::function<void()> closeFunc_ = nullptr;
+	std::function<void(corteli::base::container::Buffer<char>)> recvFunc_ = nullptr;
+};
+
